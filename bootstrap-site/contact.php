2f48100b20accb9484da2526881bff77a6d5d716
Brendan Slevin <bslevin87@gmail.com>
2014-12-16 21:02:45 -0700

    personal website work

---
+++ bootstrap-site/contact.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="../../favicon.ico">
+
+	<title>Brendan Slevin</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css1.css" rel="stylesheet" type="text/css">
+
+	<!-- Custom styles for this template -->
+	<link href="navbar-static-top.css" rel="stylesheet">
+
+	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+	<script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+
+<!-- Static navbar -->
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Brendan Slevin</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#about">About Me</a></li>
+				<li><a href="">My Resume</a></li>
+				<li><a href="#">My Portfolio</a></li>
+				<li><a href="#">Contact Me</a></li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div>
+</nav>
+</body>
+</html>
